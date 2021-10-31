b7f050eba186ab27d607ac60629195bc2d100f85
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-31 22:35:23 +0530

    REFACTORED: Proper indentation is followed

--- src/main/java/com/bridgelabz/springrestcallsdemo/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/springrestcallsdemo/controller/HelloWorldController.java
@@ -13,39 +13,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
 @RestController
 @RequestMapping("/hello")
-public class HelloWorldController 
-{
-    @RequestMapping(value = {"","/","/home"})
-    public String sayHello()
-    {
-        return "Hello from bridgelabz!";
-    }
-    
-    @RequestMapping(value="/query", method=RequestMethod.GET)
-    public String sayHello(@RequestParam(value="name") String name) 
-    {
-        return "Hello "+name+"!";
-    }
-    	
-    @GetMapping("/param/{name}")
-    public String sayHelloParam(@PathVariable String name)
-    {
-        return "Hello "+name+"!";
-    }
-    
-    @PostMapping("/post")
-    public String sayHello(@RequestBody User user)
-    {
+public class HelloWorldController {
+	@RequestMapping(value = { "", "/", "/home" })
+	public String sayHello() {
+		return "Hello from bridgelabz!";
+	}
 
-        return "Hello "+user.getFirstName()+" "+user.getLastName();
-    }
-    @PutMapping("put/{firstName}")
-    public String sayHello(@PathVariable String firstName, @RequestParam(value="lastName") String lastName) {
+	@RequestMapping(value = "/query", method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + "!";
+	}
 
-        return "Hello "+firstName+" "+lastName+"!";
-    }
+	@GetMapping("/param/{name}")
+	public String sayHelloParam(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+
+		return "Hello " + user.getFirstName() + " " + user.getLastName();
+	}
+
+	@PutMapping("put/{firstName}")
+	public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+
+		return "Hello " + firstName + " " + lastName + "!";
+	}
 }
