8002f7e5109f90a7d3288c855f2f8e2c4a1c68cd
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-31 21:14:26 +0530

    ADDED: Initial files

---
+++ src/main/java/com/bridgelabz/springrestcallsdemo/SpringRestCallsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springrestcallsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCallsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestCallsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/springrestcallsdemo/SpringRestCallsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springrestcallsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCallsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
